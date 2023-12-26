f8a71b358a73d8539199267f0371785b9a0eab68
Reis567 <matheusdosreis9@gmail.com>
2023-12-26 14:47:01 -0300

    Room entity

---
+++ src/entities/Room.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('rooms')
+export class Room {
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @Column({type:'text'})
+    name:string
+
+
+}
