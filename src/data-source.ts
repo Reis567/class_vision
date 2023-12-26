f48145b12bb8934e298f6421bf61ff44e183cf46
Reis567 <matheusdosreis9@gmail.com>
2023-12-26 07:56:41 -0300

    initial commit

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import'reflect-metadata'
+import 'dotenv/config'
+import { DataSource } from "typeorm";
+
+
+const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432; 
+export const AppDataSource = new DataSource({
+    type:"postgres",
+    host:process.env.DB_HOST,
+    port:port,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+
+}) 
