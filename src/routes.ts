5b20df9aea9a59016265c4d702c77ef76c1965b4
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 07:19:26 -0300

    create video route

--- src/routes.ts
+++ src/routes.ts
@@ -10,4 +10,4 @@ routes.post('/subject', new SubjectController().create)
 routes.post('/room', new RoomController().create)
 
 
-routes.post('/room/:idRoom/create')
+routes.post('/room/:idRoom/create', new RoomController().createVideo)
