c183218afa358364a61674cb24315887ff92c92b
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 07:02:31 -0300

    method create - route from room

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { SubjectController } from "./controllers/SubjectControllers";
+import { RoomController } from "./controllers/RoomController";
 
 export const routes = Router()
 
 routes.post('/subject', new SubjectController().create)
+
+
+routes.post('/room', new RoomController().create)
