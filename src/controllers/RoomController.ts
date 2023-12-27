6061e43df414c123be9350e59a8b1993184d7bbe
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 06:58:19 -0300

    error catch in room controller

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
 export class RoomController {
     async create(req:Request,res:Response){
         const{name, description} = req.body
+
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message:'Internal server error'
+            })
+        }
 
     }
 }
