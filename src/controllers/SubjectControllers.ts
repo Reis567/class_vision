a62e23c145fe7aefc7b3194c8b17d9b04b8ec1fe
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 06:45:14 -0300

    create method using repository

--- src/controllers/SubjectControllers.ts
+++ src/controllers/SubjectControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
+import { subjectRepository } from "../repositories/SubjectRepository"
 
 
 export class SubjectController {
@@ -10,6 +11,9 @@ export class SubjectController {
             return res.status(StatusCodes.BAD_REQUEST).json({mensagem:'o nome é obrigatório !'})
         }
         try {
+            const newSubject = subjectRepository.create({name})
+            await subjectRepository.save(newSubject)
+            return res.status(StatusCodes.CREATED).json(newSubject)
             
         } catch (error) {
             console.log(error)
