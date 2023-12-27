8dbc5ef65b28f267fc09e7f862a7208294a5e518
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 06:41:35 -0300

    repository for subject

--- src/controllers/SubjectControllers.ts
+++ src/controllers/SubjectControllers.ts
@@ -1,5 +1,19 @@
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+
+
 export class SubjectController {
-    async create(){
-        
+    async create(req:Request,res:Response){
+        const{name}= req.body
+
+        if(!name){
+            return res.status(StatusCodes.BAD_REQUEST).json({mensagem:'o nome é obrigatório !'})
+        }
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({mensagem:"Internal server error"})
+        }
     }
 }
