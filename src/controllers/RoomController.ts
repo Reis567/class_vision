5691df5c9f1f887dbd8465a3d50e3fe0ab00520b
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 07:36:17 -0300

    method to create relation between room and subject

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { roomRepository } from "../repositories/RoomRepository";
 import { videoRepository } from "../repositories/VideoRepository";
+import { subjectRepository } from "../repositories/SubjectRepository";
 
 export class RoomController {
     async create(req:Request,res:Response){
@@ -49,5 +50,35 @@ export class RoomController {
             })
         }
 
+    }
+
+    async roomSubject(req:Request,res:Response){
+        const{subject_id} = req.body
+        const {idRoom} = req.params
+
+        try {
+            const room = await roomRepository.findOneBy({id:Number(idRoom)})
+            if(!room){
+                return res.status(StatusCodes.NOT_FOUND).json({message:'Aula não existe'})
+            }
+
+            const subject = await subjectRepository.findOneBy({id:Number(subject_id)})
+
+            if(!subject){
+                return res.status(StatusCodes.NOT_FOUND).json({message:'Disciplina não existe'})
+            }
+
+            await roomRepository.update(idRoom,{
+                ...room,
+                subjects:[subject]
+            })
+
+            return res.status(StatusCodes.OK).json(room)
+        } catch (error) {
+            console.log(error)
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message:'Internal server error'
+            })
+        }
     }
 }
