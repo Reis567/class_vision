245f99dfd7d099f5f6a3e090d7d8789832e6af0e
Reis567 <matheusdosreis9@gmail.com>
2023-12-27 06:14:58 -0300

    console log to path of server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import express from 'express'
-import { AppDataSource } from './data-source'
-import { routes } from './routes'
-AppDataSource.initialize().then(()=>{
+import express from 'express';
+import { AppDataSource } from './data-source';
+import { routes } from './routes';
 
-    const app = express()
+AppDataSource.initialize().then(() => {
+  const app = express();
 
-    app.use(express.json())
+  app.use(express.json());
+  app.use(routes);
 
-    app.use(routes)
+  const port = process.env.PORT || 3000;
 
-    return app.listen(process.env.PORT)
-})
+  const server = app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+
+  // Adicione um listener para o evento 'close' para imprimir uma mensagem ao encerrar o servidor
+  server.on('close', () => {
+    console.log('Server closed');
+  });
+
+  return server;
+});
