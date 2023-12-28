92d6e05fb5151878f5a8b52dc26e15c9af021de3
Reis567 <matheusdosreis9@gmail.com>
2023-12-28 17:06:12 -0300

    removed the jest by bugs

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,25 @@ import { AppDataSource } from './data-source';
 import { routes } from './routes';
 import { ErrorMiddleware } from './middlewares/Error';
 
-const initializeApp = async () => {
-  await AppDataSource.initialize();
-
+AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
   app.use(routes);
-  app.use(ErrorMiddleware);
+  app.use(ErrorMiddleware)
+
+
 
   const port = process.env.PORT || 3000;
 
   const server = app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
   });
+
+  // Adicione um listener para o evento 'close' para imprimir uma mensagem ao encerrar o servidor
   server.on('close', () => {
     console.log('Server closed');
   });
 
-  return app;
-};
-
-export default initializeApp();
+  return server;
+})
--- tests/Room/GetAll.test.ts
+++
@@ -1,9 +0,0 @@
-import { testServer } from "../jest.setup";
-
-describe('Testes de integração', () => {
-  it('Deve retornar status 200 para a rota raiz', async () => {
-    const response = await testServer.get('/');
-    expect(response.status).toBe(200);
-  });
-
-});
