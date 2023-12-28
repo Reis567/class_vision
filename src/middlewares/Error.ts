d9493129454760e37640b83477ac631685f4ff58
Reis567 <matheusdosreis9@gmail.com>
2023-12-28 06:18:59 -0300

    logging on error middleware

--- src/middlewares/Error.ts
+++ src/middlewares/Error.ts
@@ -2,14 +2,19 @@ import express, { NextFunction ,Request,Response} from 'express';
 import { ApiError } from '../helpers/api-errors';
 
 export const ErrorMiddleware = (
-        error:Error&Partial<ApiError>,
-        req:Request,
-        res:Response,
-        next:NextFunction
-    )=>{
+  error: Error & Partial<ApiError>,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode ?? 500;
+  const message = error.statusCode ? error.message : 'Erro interno do servidor';
 
-    const statusCode = error.statusCode ?? 500
-    const message = error.statusCode ? error.message : 'Internal server Error'
-    return res.status(statusCode).json({message:message})
+  
+  console.error(`Error: ${statusCode} - ${message}`, { error, request: req });
 
-  }
+  res.status(statusCode)
+      .json({ message })
+      .header('Content-Type', 'application/json')
+      .header('X-Content-Type-Options', 'nosniff');
+};
