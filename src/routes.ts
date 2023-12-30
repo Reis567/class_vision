819fcd062a7bd88f2169468d9d13544d882aa6a5
Reis567 <matheusdosreis9@gmail.com>
2023-12-30 19:51:31 -0300

    routes comments

--- src/routes.ts
+++ src/routes.ts
@@ -7,54 +7,119 @@ import { UserController } from "./controllers/UserController";
 import { AuthMiddleware } from "./middlewares/authMiddleware";
 
 export const routes = Router()
-
-routes.get('/',(req, res)=>{
-    res.send('Bem vindo a API')
-})
-
-//User routes
-
-//Create
-routes.post('/user', new UserController().create)
-//Login 
-routes.post('/login',new UserController().login)
-//Get Profile
-routes.get('/profile',AuthMiddleware,new UserController().getProfile)
-
-
-//Room routes
-
-//GetAll
-routes.get('/room',AuthMiddleware, new RoomController().list)
-//Create
-routes.post('/room',AuthMiddleware, new RoomController().create)
-// Update by id
-routes.put('/room/:id',AuthMiddleware, new RoomController().updateById);
-// Get by ID
-routes.get('/room/:id',AuthMiddleware, new RoomController().getById);
-// Delete by ID
-routes.delete('/room/:id',AuthMiddleware, new RoomController().deleteById);
-
-
-
-//Subject routes
-//Create
-routes.post('/subject',AuthMiddleware, new SubjectController().create)
-//GetAll
-routes.get('/subject',AuthMiddleware, new SubjectController().getAll)
-// Update by id
-routes.put('/subject/:id',AuthMiddleware, new SubjectController().updateById);
-// Delete by ID
-routes.delete('/subject/:id',AuthMiddleware, new SubjectController().deleteById);
-// Get by ID
-routes.get('/subject/:id',AuthMiddleware, new SubjectController().getById);
-
-
-
-
-
-
-
-//Room relations routes
-routes.post('/room/:idRoom/create',AuthMiddleware, new RoomController().createVideo)
-routes.post('/room/:idRoom/subject',AuthMiddleware, new RoomController().roomSubject)
+/**
+ * @route GET /
+ * @description Welcome to the API
+ * @access Public
+ */
+routes.get("/", (req, res) => {
+    res.send("Bem-vindo a ClassVision API");
+  });
+  
+  // User Routes
+  /**
+   * @route POST /user
+   * @description Create a new user
+   * @access Public
+   */
+  routes.post("/user", new UserController().create);
+  
+  /**
+   * @route POST /login
+   * @description User login
+   * @access Public
+   */
+  routes.post("/login", new UserController().login);
+  
+  /**
+   * @route GET /profile
+   * @description Get user profile
+   * @access Private (requires authentication)
+   */
+  routes.get("/profile", AuthMiddleware, new UserController().getProfile);
+  
+  // Room Routes
+  /**
+   * @route GET /room
+   * @description Get all rooms
+   * @access Private (requires authentication)
+   */
+  routes.get("/room", AuthMiddleware, new RoomController().list);
+  
+  /**
+   * @route POST /room
+   * @description Create a new room
+   * @access Private (requires authentication)
+   */
+  routes.post("/room", AuthMiddleware, new RoomController().create);
+  
+  /**
+   * @route PUT /room/:id
+   * @description Update a room by ID
+   * @access Private (requires authentication)
+   */
+  routes.put("/room/:id", AuthMiddleware, new RoomController().updateById);
+  
+  /**
+   * @route GET /room/:id
+   * @description Get a room by ID
+   * @access Private (requires authentication)
+   */
+  routes.get("/room/:id", AuthMiddleware, new RoomController().getById);
+  
+  /**
+   * @route DELETE /room/:id
+   * @description Delete a room by ID
+   * @access Private (requires authentication)
+   */
+  routes.delete("/room/:id", AuthMiddleware, new RoomController().deleteById);
+  
+  /**
+   * @route POST /room/:idRoom/create
+   * @description Create a video in a specific room
+   * @access Private (requires authentication)
+   */
+  routes.post("/room/:idRoom/create", AuthMiddleware, new RoomController().createVideo);
+  
+  /**
+   * @route POST /room/:idRoom/subject
+   * @description Associate a room with a subject
+   * @access Private (requires authentication)
+   */
+  routes.post("/room/:idRoom/subject", AuthMiddleware, new RoomController().roomSubject);
+  
+  // Subject Routes
+  /**
+   * @route POST /subject
+   * @description Create a new subject
+   * @access Private (requires authentication)
+   */
+  routes.post("/subject", AuthMiddleware, new SubjectController().create);
+  
+  /**
+   * @route GET /subject
+   * @description Get all subjects
+   * @access Private (requires authentication)
+   */
+  routes.get("/subject", AuthMiddleware, new SubjectController().getAll);
+  
+  /**
+   * @route PUT /subject/:id
+   * @description Update a subject by ID
+   * @access Private (requires authentication)
+   */
+  routes.put("/subject/:id", AuthMiddleware, new SubjectController().updateById);
+  
+  /**
+   * @route DELETE /subject/:id
+   * @description Delete a subject by ID
+   * @access Private (requires authentication)
+   */
+  routes.delete("/subject/:id", AuthMiddleware, new SubjectController().deleteById);
+  
+  /**
+   * @route GET /subject/:id
+   * @description Get a subject by ID
+   * @access Private (requires authentication)
+   */
+  routes.get("/subject/:id", AuthMiddleware, new SubjectController().getById);
